db6635527f4fa590e2dcd301d7cb9a97efdae597
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2021-07-25 19:29:56 -0400

    distingushing between an new and reopened study

--- SonaNotification.py
+++ SonaNotification.py
@@ -22,7 +22,9 @@ class SonaNotification:
         self._email_password = credentials[3]
         connection_string = f"mongodb+srv://{credentials[4]}:{credentials[5]}@cluster0.cthkj.mongodb.net/{credentials[6]}?retryWrites=true&w=majority"
         self._db = pymongo.MongoClient(connection_string)
-        self._collection = self._db.Sona.Studies
+        self._collection = self._db[credentials[6]]
+        self._current_list = credentials[7]
+        self._master_list = credentials[8]
 
     def not_time_slot_label(self, label):
         return label and not re.compile("Timeslots Available").search(label)
@@ -44,8 +46,9 @@ class SonaNotification:
           for child in td.findAll('a', attrs={'class': False}):
             current_studies.append(child.string.lower().strip())
 
-        old_studies = self.getOldStudies()
+        old_studies = self.getOldStudies(False)
         differences, studies_not_displayed_anymore = self.compareList(current_studies, old_studies)
+        master_list_studies = self.getOldStudies(True)
 
         # print("Old Studies: ", old_studies)
         # print("Current Studies on Sona: ", current_studies)
@@ -54,24 +57,31 @@ class SonaNotification:
 
         if len(differences) != 0:
           for new_study in differences:
-            self.sendEmail(new_study)
-            self.updateDB(new_study)
-            self.log("Email", "Old Studies: " + ", ".join(old_studies) + "; Current Studies: " + ", ".join(current_studies) + "; Differences: " + ", ".join(differences))
+            reopened_study = new_study in master_list_studies
+            self.sendEmail(new_study, reopened_study)
+            self.updateDB(new_study, False)
+            if not reopened_study:
+                self.updateDB(new_study, True)
+            self.log("Email", "Old Studies: " + ", ".join(old_studies) + "; Current Studies: " + ", ".join(current_studies) + "; Differences: " + ", ".join(differences) + "; Master List: " + ", ".join(master_list_studies))
         self.deleteInDB(studies_not_displayed_anymore)
         return
 
-    def getOldStudies(self):
-        current_studies = []
-        collection_data = self._collection.find({}, {'name': 1, '_id': 0})
+    def getOldStudies(self, is_master_list):
+        studies = []
+        collection_name =  self._master_list if is_master_list else self._current_list
+        collection_data = self._collection[collection_name].find({}, {'name': 1, '_id': 0})
         for study in loads(dumps(collection_data)):
-          current_studies.append(study['name'])
-        return current_studies
+          studies.append(study['name'])
+        return studies
 
-    def sendEmail(self, name_of_study):
+    def sendEmail(self, name_of_study, is_new_study):
         try:
             msg = EmailMessage()
             mailinglist = self.readFile("mailinglist.txt")
-            msg.set_content(f"There is a new study called {name_of_study}. If you have already done it in the past please ignore this email.")
+            email = f"There is a new study called {name_of_study}. If you have already done it in the past please ignore this email."
+            if not is_new_study:
+                email = f"An old study called {name_of_study} has been reopened. If you have already done it in the past please ignore this email."
+            msg.set_content(email)
             msg['Subject'] = "New Sona Study Notification"
             msg['From'] = "donotreply@gmail.com"
             msg['To'] = ', '.join(mailinglist)
@@ -85,13 +95,14 @@ class SonaNotification:
 
         return
 
-    def updateDB(self, study):
-        self._collection.insert_one({'name': study})
+    def updateDB(self, study, is_master_list):
+        collection_name =  self._master_list if is_master_list else self._current_list
+        self._collection[collection_name].insert_one({'name': study})
         return
 
     def deleteInDB(self, studies):
         for study in studies:
-            self._collection.delete_one({'name': study})
+            self._collection[self._current_list].delete_one({'name': study})
         return 
 
     def compareList(self, newlist, oldlist):
